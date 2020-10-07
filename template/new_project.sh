e0c517788a2d879bdaa1307eea7d82d21cda74e0
Hai Nguyen <hainguyen.eeit@gmail.com>
2020-10-07 05:36:09 -0700

    add folder component to project template, beautify the script

--- template/new_project.sh
+++ template/new_project.sh
@@ -2,21 +2,29 @@
 
 #author: Quang Hai Nguyen
 #version: 0.99
-
-PrintHelp()
-{
-    echo "Please type ./new_project.sh <project_name> to create a new template"
-}
-
-
+echo
+echo
+echo "#################################"
+echo "# esp32 project template"
+echo "# author: Quang Hai Nguyen"
+echo "# version: 0.99"
+echo "#################################"
+echo
 if [ -z $1 ] || [ -z $2 ]
 then
-    PrintHelp
+    echo "Please type ./new_project.sh <project_name> <file_path> to create a new template"
 else
-    echo create new project $1 at $2
-    cp -r hello_world $2
-    mv $2/hello_world $2/$1
-    sed -i -e "s/hello-world/$1/g" $2/$1/CMakeLists.txt
-    sed -i -e "s/hello-world/$1/g" $2/$1/Makefile
+    if [ -z $IDF_PATH ]
+    then
+        echo "please source the SDK path" 
+    else
+        echo create new project $1 at $2
+        cp -r hello_world $2
+        mv $2/hello_world $2/$1
+        sed -i -e "s/hello-world/$1/g" $2/$1/CMakeLists.txt
+        sed -i -e "s/hello-world/$1/g" $2/$1/Makefile
+    fi
 fi
+echo
+echo
 
